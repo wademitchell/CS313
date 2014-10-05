820ec80856b6dffcef16792b2ea376aa7779a693
wademitchell <wademitchell>
2014-10-04 23:56:09 -0700

    res

---
+++ survey/poll_vote_read.php
@@ -0,0 +1,32 @@
+<?php
+//get content of textfile
+$filename = "poll_result.txt";
+$content = file($filename);
+
+$array = explode("||", $content[0]);
+$yes = $array[0];
+$no = $array[1];
+
+?>
+
+<h2>Result:</h2>
+<table>
+<tr>
+<td>Yes:</td>
+<td>
+<img src="poll.gif"
+width='<?php echo(100*round($yes/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($yes/($no+$yes),2)); ?>%
+</td>
+</tr>
+<tr>
+<td>No:</td>
+<td>
+<img src="poll.gif"
+width='<?php echo(100*round($no/($no+$yes),2)); ?>'
+height='20'>
+<?php echo(100*round($no/($no+$yes),2)); ?>%
+</td>
+</tr>
+</table>
